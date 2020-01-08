ed0d5351d78814260d994452f0f55fb99fe5e1d5
ashwini ramchandra kamble <ashwinik695@gmail.com>
2020-01-08 12:03:41 +0530

    Upload latest files of mail functionality.
    Send mail with proper log generate not stuck in any case

--- src/main/com/premiersolutionshi/mail/DBSchedular.java
+++ src/main/com/premiersolutionshi/mail/DBSchedular.java
@@ -3,83 +3,88 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
+/**
+* @author Ashwini
+* @version 1.0, 4-Dec-2019
+*/ 
 
 public class DBSchedular{
 	
 	public static final DateTimeFormatter COMMON_BASIC_FORMAT_DATE = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-    private static Logger logger = Logger.getLogger(DBSchedular.class.getSimpleName());
-    String getNowInBasicFormatDate() {
+ //   private static Logger logger = Logger.getLogger(DBSchedular.class.getSimpleName());
+	private static Logger logger = Logger.getLogger("MailLogger");
+	
+	//convert date to basic format
+   String getNowInBasicFormatDate() {
  	    return LocalDate.now().format(COMMON_BASIC_FORMAT_DATE);
 	}
 
   public Collection<String> connect()
 	{
+	  //list of email address
 		ArrayList  mailTo=new ArrayList();
-		// String[] mailTo;
-		User resultBean = new User();
+		UserInfo resultBean = new UserInfo();
 				try{
-					
 			    Connection conn=null;
 			    try
 			    {
-                Class.forName("org.sqlite.JDBC");  
+                Class.forName("org.sqlite.JDBC");  											//database connection
 				String url = "jdbc:sqlite:/C:/sqlite3_db/pshi.db";
 				conn = DriverManager.getConnection(url);
 				logger.info("Successfully Connected to Database");
-
 				System.out.println("Connection Got it!");
-
 			    }catch(Exception e){
-			    	logger.error("Error in Database Connectivity", e);
-
+			    	logError("DBSchedular | connect","Error in Database Connectivity", e);
 			    }
+			    
 				String date=getNowInBasicFormatDate();
 	    	    String sqlStmt="SELECT user_fk,first_name, last_name,email, start_date_fmt, end_date_fmt, leave_type, location FROM pto_travel_vw";
+	    	    //execute query
 	    	    PreparedStatement pStmt=conn.prepareStatement(sqlStmt);
 			    ResultSet rs = pStmt.executeQuery();
 			     int size=0;
-			   while(rs.next())
+			     while(rs.next())
 			      {
+			    	 //check travel end date equals to current date and travel type is "Travel"
 				  if(rs.getString("end_date_fmt").equals(date) && rs.getString("leave_type").equals("Travel"))
 				  {
 					  size++;
 					  String email=rs.getString("email");
+					  
+					  //check email ID is null or not
 					  if(email==null || email.equals(""))
-					  {
-						  logger.error("No email ID provided for user "+rs.getString("first_name")+" "+rs.getString("last_name"));
-			    	  }
-					  else{
-					  resultBean.setEmail(rs.getString("email"));
-					  mailTo.add(resultBean.getEmail());
+					  	{
+						  logError("DBSchedular | connect","No email ID provided for user"+rs.getString("first_name")+" "+rs.getString("last_name"));
+					  	}
+					  else
+					  	{
+						  //set mail
+						  resultBean.setEmail(rs.getString("email"));
+						  mailTo.add(resultBean.getEmail());
 					  	}
 				  }
-				 
-			  }
+			      }
 			  
 				}catch(Exception e){
-					logError("connect","Exception are",e);
-				}
-	return mailTo;
+					logError("DBSchedular | connect","Exception are",e);
+					}
+	return mailTo;                          //return list of e-Mail address
 	}
   
-  private void logError(String functionName, String statement) {
+ private void logError(String functionName, String statement) {
       logError(functionName, statement, null);
   }
 
-  private void logError(String functionName, String statement, Exception e) {
+ private void logError(String functionName, String statement, Exception e) {
       if (e != null) {
           logger.error(String.format("%11s%-30s | %-34s | %s", "", "ERROR", functionName, statement), e);
           e.printStackTrace();
---
+++ src/main/com/premiersolutionshi/mail/ReminderEmailSchedulerServlet.java
@@ -0,0 +1,64 @@
+package com.premiersolutionshi.mail;
+import java.util.logging.FileHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+@SuppressWarnings("serial")
+
+/**
+ * Servlet for sending reminder email to users on their last day of travel or PTO
+ *
+ * @author Ashwini
+ * @version 1.0, 2-Dec-2019
+ */
+
+
+public class ReminderEmailSchedulerServlet  extends HttpServlet{
+
+	static Logger logger=Logger.getLogger("MailLogger");
+	
+	public void init()	throws ServletException
+	{
+		//configure log4j properties file
+			PropertyConfigurator.configure("C:/cloaked/pshi/WEB-INF/config/log4j.properties");
+	
+			logger.info("------------------------------------------------------------------");
+			logger.info("--------------Inside TrackItEmailServlet init() method -----------");
+			logger.info("------------------------------------------------------------------");
+			System.out.println("------------------------------------------------------------------");
+			System.out.println("---------- TrackItEmailServlet Initialized successfully ----------");
+			System.out.println("------------------------------------------------------------------");
+          try
+          		{
+        	  //Execute Schedular in servlet
+         	 	 JobDetail job = JobBuilder.newJob(JobSchedular.class)
+      			.withIdentity("JobSchedule", "group1").build();
+         	 	 Trigger trigger = TriggerBuilder.newTrigger()
+  .withSchedule(CronScheduleBuilder.cronSchedule("30 * 11-18 * * ?")).build();   // TZ="US/Hawaii" //setup time to execute code
+      	//schedule it
+      	Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+      	scheduler.start();
+      	scheduler.scheduleJob(job, trigger);
+
+      }catch(Exception e)
+      	{
+      	logger.error(e);
+      	}
+          
+  		logger.info("----------- TrackItEmailServlet End of init() menthod for the hour ------------------------");
+
+      }
+
+	}	
+
